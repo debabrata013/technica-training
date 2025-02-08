6b3e775e42fc61c0d5678949bc108fa64775e3cf
DEV <pattnaikd833@gmail.com>
2025-02-08 17:50:36 +0530

    best time to buy the stock

--- cw/A.java
+++ cw/A.java
@@ -41,16 +41,33 @@ public class A {
         sb.append(s1);
         System.out.println(sb);
     }
-    
+    static void profit(int price[],int n){
+        int profit=0;
+        int bd=price[0];
+        for(int i=1;i<n;i++){
+           if(price[i]>bd){
+                profit=Math.max(price[i]-bd,profit);
+           }else{
+                bd=price[i];
+           }
+        }
+        System.out.println(profit);
+    }
+
     public static void main(String[] args) {
         // int r=7;
         // int unit=2;
         // int arr[]={2,8,3,5,7,4,1,2};
         // int n=arr.length;
         // q1(r,unit,arr,n);
-        String s="#abc#d#dev";
-        int n=s.length();
-        print(s,n);
+        // String s="#abc#d#dev";
+        // int n=s.length();
+        // print(s,n);
+        int a[]={7,1,5,3,6,4};
+        // int a[]={7,6,4,3,1};
+        int n=a.length;
+        profit(a,n);
+        
          
     }
     
