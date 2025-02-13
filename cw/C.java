9c84de6d9ace5b3252f5aa566dfd17c9f2efd2a6
DEV <pattnaikd833@gmail.com>
2025-02-13 15:55:07 +0530

    Implement kangaru function to determine if two kangaroos meet based on their starting positions and velocities

--- cw/C.java
+++ cw/C.java
@@ -21,10 +21,41 @@ class C{
         }
         return ans;
     }
+    static void kangaru(int x1,int x2,int v1,int v2){
+       if(x1==x2){
+           System.out.println("YES");
+           return;
+       }
+       if(v1==v2){
+
+              System.out.println("NO");
+              return;
+         }
+    
+    
+        if(x1<x2 && v1<=v2){
+            System.out.println("NO");
+            return;
+        }
+        if(x1>x2 && v1>=v2){
+            System.out.println("NO");
+            return;
+        }
+        if((x2-x1)%(v1-v2)==0){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+
+       
+        
+    }
     public static void main(String[] args) {
-        int arr[]={1,3,4,5,6};
-        ArrayList<Integer> ans=icecrem(6,arr);
-        System.out.println(ans);
+        // int arr[]={1,3,4,5,6};
+        // ArrayList<Integer> ans=icecrem(6,arr);
+        // System.out.println(ans);
+        kangaru(0, 4, 3, 2);
 
     }
 }
