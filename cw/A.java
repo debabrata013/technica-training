7b32332e33ae9095a8fc496ae10888c58ecc5408
DEV <pattnaikd833@gmail.com>
2025-02-16 21:20:40 +0530

    code mofied

--- cw/A.java
+++ cw/A.java
@@ -1,74 +1,78 @@
 package cw;
 
 public class A {
-    static void q1(int r,int unit,int arr[],int n){
-        if(arr.length==0){
+    // Method to calculate the minimum number of days to meet the food requirement
+    static void q1(int requiredDays, int unit, int[] foodArray, int n) {
+        if (foodArray.length == 0) {
             System.out.println("-1");
             return;
         }
-        int foodreq=r*unit;
-        int food=0,c=0;
-        for(int i=0;i<n;i++){
-            if(food<=foodreq){
-                food+=arr[i];
-                c++;
-            }
-            else{
+        int foodRequirement = requiredDays * unit;
+        int food = 0, days = 0;
+        for (int i = 0; i < n; i++) {
+            if (food <= foodRequirement) {
+                food += foodArray[i];
+                days++;
+            } else {
                 break;
             }
-           
-            }
-            if(food<foodreq){
-                System.out.println("0");
-            }
-            else{
-                System.out.println(c);
-            }
+        }
+        if (food < foodRequirement) {
+            System.out.println("0");
+        } else {
+            System.out.println(days);
+        }
     }
-    static void print(String s,int n){
-        int c=0;
-        for(int i=0;i<n;i++){
-            if(s.charAt(i)=='#'){
-                c++;
+
+    // Method to move all '#' characters to the beginning of the string
+    static void print(String s, int n) {
+        int hashCount = 0;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '#') {
+                hashCount++;
             }
         }
-        StringBuffer sb=new StringBuffer();
-        for(int i=0;i<c;i++){
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < hashCount; i++) {
             sb.append("#");
         }
 
-        String s1=s.replace("#","");
+        String s1 = s.replace("#", "");
         sb.append(s1);
         System.out.println(sb);
     }
-    static void profit(int price[],int n){
-        int profit=0;
-        int bd=price[0];
-        for(int i=1;i<n;i++){
-           if(price[i]>bd){
-                profit=Math.max(price[i]-bd,profit);
-           }else{
-                bd=price[i];
-           }
+
+    // Method to calculate the maximum profit from buying and selling stock
+    static void profit(int[] prices, int n) {
+        int maxProfit = 0;
+        int minPrice = prices[0];
+        for (int i = 1; i < n; i++) {
+            if (prices[i] > minPrice) {
+                maxProfit = Math.max(prices[i] - minPrice, maxProfit);
+            } else {
+                minPrice = prices[i];
+            }
         }
-        System.out.println(profit);
+        System.out.println(maxProfit);
     }
 
     public static void main(String[] args) {
-        // int r=7;
-        // int unit=2;
-        // int arr[]={2,8,3,5,7,4,1,2};
-        // int n=arr.length;
-        // q1(r,unit,arr,n);
-        // String s="#abc#d#dev";
-        // int n=s.length();
-        // print(s,n);
-        int a[]={7,1,5,3,6,4};
-        // int a[]={7,6,4,3,1};
-        int n=a.length;
-        profit(a,n);
-        
-         
+        // Example usage of q1 method
+        // int r = 7;
+        // int unit = 2;
+        // int[] arr = {2, 8, 3, 5, 7, 4, 1, 2};
+        // int n = arr.length;
+        // q1(r, unit, arr, n);
+
+        // Example usage of print method
+        // String s = "#abc#d#dev";
+        // int n = s.length();
+        // print(s, n);
+
+        // Example usage of profit method
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        // int[] prices = {7, 6, 4, 3, 1};
+        int n = prices.length;
+        profit(prices, n);
     }
-    
 }
