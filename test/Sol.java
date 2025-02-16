b205fa45fb94f0f93db2cba03e067cf4aaca0bed
DEV <pattnaikd833@gmail.com>
2025-02-17 01:45:38 +0530

    code optimized

--- test/Sol.java
+++ test/Sol.java
@@ -3,89 +3,95 @@ package test;
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class Sol{
-    static void q1(String a){
-        int n=a.length();
-       
-            // map in java
-            HashMap<Character, Integer> map = new HashMap<>();
-            for(int i=0;i<n;i++){
-                if(map.containsKey(a.charAt(i))){
-                    map.put(a.charAt(i), map.get(a.charAt(i))+1);
-                }
-                else{
-                    map.put(a.charAt(i), 1);
-                }
-            }
-
-int odd=0;
-for(int i: map.values()){
-    if(i%2!=0){
-        odd++;
-    }}
-    if(odd==0||odd==1){
-        System.out.println("YES");
-    }
-    else{
-        System.out.println("NO");
+public class Sol {
+    // Method to check if a string can be rearranged to form a palindrome
+    static void q1(String str) {
+        int n = str.length();
+        HashMap<Character, Integer> map = new HashMap<>();
         
-    }
-}
+        // Count the frequency of each character
+        for (int i = 0; i < n; i++) {
+            char ch = str.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
 
-static void q2(int c,int w){
-  
-    int day=0;
-    c+=w/7;
-    while(c>0){
-        day++;
-        w++;
-        c--;
-        if(w%7==0){
-c++;
-            w=w%7;
+        int oddCount = 0;
+        // Count the number of characters with odd frequency
+        for (int count : map.values()) {
+            if (count % 2 != 0) {
+                oddCount++;
+            }
+        }
+
+        // A string can be rearranged to form a palindrome if it has at most one character with an odd frequency
+        if (oddCount == 0 || oddCount == 1) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
         }
     }
-    System.out.println(day);
-}
-static void q3(int []   a){
-    int l=0,r=a.length-1;
-    int max=0;
-    while(l<=r){
-        int h=Math.min(a[l], a[r]);
-        int w=r-l;
-        max=Math.max(max, h*w);
-        if(a[l]<a[r]){
-            l++;
-        }
-        else{
-            r--;
-        }
-        }
-        System.out.println(max);
-}
 
+    // Method to calculate the number of days to consume all chocolates
+    static void q2(int chocolates, int wrappers) {
+        int days = 0;
+        chocolates += wrappers / 7;
+        
+        while (chocolates > 0) {
+            days++;
+            wrappers++;
+            chocolates--;
+            if (wrappers % 7 == 0) {
+                chocolates++;
+                wrappers = wrappers % 7;
+            }
+        }
+        System.out.println(days);
+    }
+
+    // Method to find the maximum area of water that can be contained
+    static void q3(int[] heights) {
+        int left = 0, right = heights.length - 1;
+        int maxArea = 0;
+        
+        while (left <= right) {
+            int height = Math.min(heights[left], heights[right]);
+            int width = right - left;
+            maxArea = Math.max(maxArea, height * width);
+            
+            if (heights[left] < heights[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        System.out.println(maxArea);
+    }
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter the string for q1:");
-        String s=sc.nextLine();
-        System.out.println("Enter the number of chocolate  for q2:");
-        int c=sc.nextInt();
-        System.out.println("Enter the number of chocolate wraper for q2:");
-        int w=sc.nextInt();
-        System.out.println("Enter the array  size of array for q3:");
-        int n=sc.nextInt();
-        int [] a=new int[n];
+        String str = scanner.nextLine();
+
+        System.out.println("Enter the number of chocolates for q2:");
+        int chocolates = scanner.nextInt();
+
+        System.out.println("Enter the number of chocolate wrappers for q2:");
+        int wrappers = scanner.nextInt();
+
+        System.out.println("Enter the size of the array for q3:");
+        int n = scanner.nextInt();
+        int[] heights = new int[n];
+
         System.out.println("Enter the array elements for q3:");
-        for(int i=0;i<n;i++){
-            a[i]=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            heights[i] = scanner.nextInt();
         }
 
+        q1(str);
+        q2(chocolates, wrappers);
+        q3(heights);
 
-        q1(s);
-        q2(c,w);
-        q3(a);
-
-        
+        scanner.close();
     }
 }
