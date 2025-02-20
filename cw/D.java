9a19658ab0b8939e3a95fdda8c4151e257da25b2
DEV <pattnaikd833@gmail.com>
2025-02-20 15:34:05 +0530

    number of char need to delete so that the adjucent number are not same

--- cw/D.java
+++ cw/D.java
@@ -1,7 +1,17 @@
 package cw;
 
 public class D {
+    static void num(String s){
+        int count = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i-1)) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
     public static void main(String[] args) {
+    num("AABABB");
         
     }
     
