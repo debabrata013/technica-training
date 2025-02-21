206de79857a5f8c751b801385279c43a96bf7f5f
DEV <pattnaikd833@gmail.com>
2025-02-21 17:10:30 +0530

    userinput added

--- cw/E.java
+++ cw/E.java
@@ -1,4 +1,7 @@
 package cw;
+
+import java.util.Scanner;
+
 class E{
     static void angryProfesher(int n,int k,int arr[]){
         int sum=0;
@@ -8,16 +11,20 @@ class E{
             }
         }
         if(sum>=k){
-            System.out.println("No , don't cancle the class");
+            System.out.println("No");
         }
         else{
-            System.out.println("Yes, cancle the class");
+            System.out.println("Yes");
         }
     }
     public static void main(String[] args) {
-        int n=5;
-        int k=3;
-        int arr[]={-1,2,1,-3,4};
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
         angryProfesher(n,k,arr);
 
     }
