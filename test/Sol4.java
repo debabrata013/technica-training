dad46ef259ad0a8a0bd2f92972c6101c42f2200d
DEV <pattnaikd833@gmail.com>
2025-02-22 17:01:10 +0530

    median function completed

--- test/Sol4.java
+++ test/Sol4.java
@@ -1,7 +1,24 @@
 package test;
+import java.util.*;
 
 public class Sol4 {
+    static Scanner sc = new Scanner(System.in);
+    public static void median(){
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+       Arrays.sort(arr);
+       if(n%2==0){
+           System.out.println((arr[n/2]+arr[(n/2)-1])/2);
+       }else{
+           System.out.println(arr[n/2]);
+       }
+
+    }
     
+
     public static void main(String[] args) {
         
     }
