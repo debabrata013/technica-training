6af0c91bad81aaa4efd4237fed44ee88c2e50cc1
DEV <pattnaikd833@gmail.com>
2025-02-22 17:13:20 +0530

    max bird added

--- test/Sol4.java
+++ test/Sol4.java
@@ -17,6 +17,27 @@ public class Sol4 {
        }
 
     }
+
+    static void maxBird(){
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int max=0; int count=0;
+        Arrays.sort(arr);
+        for(int i=0;i<n;i++){
+            if(arr[i]==arr[i+1]){
+                count++;
+            }else{
+                count=0;
+            }
+            if(count>max){
+                max=count;
+            }
+        }
+        System.out.println(max);
+    }
     
 
     public static void main(String[] args) {
